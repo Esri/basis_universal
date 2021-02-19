8a61790e6e22c088994b64ca2048a57772b1bd55
Boris <bhristov@esri.com>
2021-02-18 17:14:02 -0800

    basis_universal: Runtimecore branch (#1)
    * Create 'runtimecore' branch
    
    * Fix define to match other static objects
    
    Set _ITERATOR_DEBUG_LEVEL to 2 (instead of 1) to match other static libs.
    
    * Include missing *.c files in the build
    
    * Remove exclude line
    
    * Revert "Fix define to match other static objects"
    
    This reverts commit ef6174ff6026675481075d526cb8d13c141c7421.
    
    * PR suggested fixes
    
    * Include cctype instead of ctype.h
    
    * PR feedback fixes
    
    * Add missing define
    
    * Remove empty functions
    
    * Remove buildoptions block
    
    * Remove empty blocks

---
+++ conanfile_rtc.py
@@ -0,0 +1,24 @@
+from conans import ConanFile
+
+
+class BasisUniversalConan(ConanFile):
+    name = "basis_universal"
+    version = "v1.12"
+    url = "https://devtopia.esri.com/3rdparty/basis_universal/tree/runtimecore"
+    license = "https://github.com/Esri/basis_universal/blob/master/LICENSE"
+    description = "Basis Universal for Scene Layer Library."
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/basis_universal/"
+
+        # headers
+        self.copy("*.h*", src=base + "encoder", dst=relative + "encoder")
+        self.copy("*.h*", src=base + "transcoder", dst=relative + "transcoder")
+ 
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
